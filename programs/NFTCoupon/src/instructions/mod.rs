8c2ea071305be3be8d9fc16fe25ad672b5dbaa7f
ayushshrivastv <ayush.srivastav@icloud.com>
2025-10-23 10:00:00 +0530

    Add instruction module exports for merchant and coupon operations

---
+++ programs/NFTCoupon/src/instructions/mod.rs
@@ -0,0 +1,9 @@
+pub mod initialize_merchant;
+pub mod create_coupon;
+pub mod redeem_coupon;
+pub mod update_coupon_status;
+
+pub use initialize_merchant::*;
+pub use create_coupon::*;
+pub use redeem_coupon::*;
+pub use update_coupon_status::*;
